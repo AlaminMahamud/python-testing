75c450028a79f4f4bdf94844152fa4ac079c6b99
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-01-24 14:06:10 +0600

    add: views for bucketlist api
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- django/scotch/api/views.py
+++ django/scotch/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BucketlistSerializer
+from .models import BucketList
 
-# Create your views here.
+
+class CreateView(generics.ListCreateAPIView):
+    queryset = BucketList.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
