5c810555f3a477ee5175e0702c34ce7bb47bb7b3
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-01-24 16:53:11 +0600

    add: puppy models

--- django/real_python/puppies/models.py
+++ django/real_python/puppies/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Puppy(models.Model):
+    """
+    Puppy Model
+    Defines the attributes of a puppy
+    """
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    creaetd_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_breed(self):
+        return "{name} belongs to {breed} breed" \
+            .format(
+                name=self.name,
+                breed=self.breed
+            )
+
+    def __repr__(self):
+        return "{name} is added.".format(name=self.name)
