b4fa1845faf11a438e082804f9698a9e310421ef
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-01-24 16:31:45 +0600

    add: postgres configs in real_python settings.py
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- django/real_python/real_python/settings.py
+++ django/real_python/real_python/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'real_python.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'puppy_store_drf',
+        'USER': 'postgres',
+        'PASSWORD': 'psql',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
