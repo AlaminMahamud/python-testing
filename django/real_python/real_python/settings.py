1f6fc2eff343a5c7f7b5a952c6cf45113e55c50f
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-01-24 16:52:49 +0600

    fix: changed password

--- django/real_python/real_python/settings.py
+++ django/real_python/real_python/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'puppy_store_drf',
         'USER': 'postgres',
-        'PASSWORD': 'psql',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
